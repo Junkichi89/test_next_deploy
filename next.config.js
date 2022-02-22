2c6630034debe084339f8ecddcd6c65d5c7c379e
Jumpei Asanuma <jun64pei89@gmail.com>
2022-02-22 20:09:56 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-const removeImports = require('next-remove-imports')()
+const removeImports = require('next-remove-imports')();
 
 module.exports = removeImports({
-  assetPrefix: process.env.NODE_ENV === "production" ? "/project_name" : "",
+  reactStrictMode: true,
 })
